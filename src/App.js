78e27b8a1ef4d3a98215998a2e983a68fa3f433a
Aakash bhardwaj <54072018+Aakashbhardwaj27@users.noreply.github.com>
2020-06-10 16:54:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import data from './data';
+import './App.css';
+
+
+function App() {
+
+  const openMenu=()=>{
+    document.querySelector(".sidebar").classList.add('open');
+  }
+
+  const closeMenu=()=>{
+    document.querySelector(".sidebar").classList.remove('open');
+  }
+  return (
+    
+    <div className="grid-container">
+    <header className="header">
+        <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <a href="./index.html">amazona</a>
+        </div>
+        <div className="header-links">
+            <a href="cart.html">Cart  </a>
+            <a href="signin.html">Sign in</a>
+        </div>
+    </header>
+    <aside className="sidebar">
+        <h3>Shopping categorey</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+        <ul>
+           
+               <li> <a href="index.html">pants</a></li>
+               <li> <a href="index.html">shirts</a></li>
+               <li> <a href="index.html">shoes</a></li>
+        
+        </ul>
+    </aside>
+  
+    <main className="main">
+        <div className="content">
+            <ul className="products">
+              {
+                data.products.map(product=>
+                  
+                  <li>
+                  <div className="product">
+                      <img className="image" src={product.image} alt="product" />
+                      <div className="product-name">
+                          <a href="product.html" >{product.name}</a></div>
+                      <div className="product-brand">{product.brand}</div>
+                <div className="product-price">{product.price}</div>
+                <div className="product-rating">{product.rating} Stars({product.numReviews}Reviews)</div>
+
+                  </div>
+              </li>
+                  )
+              }
+               
+
+              
+               
+            </ul>
+        </div>
+       
+    </main>
+    <footer className="footer">
+        All rights reserved.
+    </footer>
+</div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export default  {
+    products:[
+        {
+        name: 'slim Shirt',
+        image:'/images/product 1.png',
+        price:60,
+        brand: 'Nike',
+        rating:4.5,
+        numReviews:100
+    },
+    {
+        name: 'slim Shirt',
+        image:'/images/product 1.png',
+        price:160,
+        brand: 'Nike',
+        rating:2.5,
+        numReviews:100
+    },
+    {
+        name: 'slim Shirt',
+        image:'/images/product 1.png',
+        price:50,
+        brand: 'Nike',
+        rating:3.5,
+        numReviews:60
+    },
+    {
+        name: 'slim Shirt',
+        image:'/images/product 1.png',
+        price:80,
+        brand: 'Nike',
+        rating:4,
+        numReviews:90
+    },
+    {
+        name: 'slim Shirt',
+        image:'/images/product 1.png',
+        price:80,
+        brand: 'Nike',
+        rating:4,
+        numReviews:90
+    },
+    {
+        name: 'slim Shirt',
+        image:'/images/product 1.png',
+        price:80,
+        brand: 'Nike',
+        rating:4,
+        numReviews:90
+    },
+    {
+        name: 'shweta',
+        image:'/images/product 1.png',
+        price:180,
+        brand: 'Mike',
+        rating:5,
+        numReviews:190
+    }
+]
+}
